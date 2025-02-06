af425c58ca0620698e5d81c75a2293f914f01a92
vishwa6990 <vishwakumar.ks@freshworks.com>
2025-02-06 16:59:16 +0530

    Rename files

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/auction-websites/'
+  base: '/hack-for-charity/'
 })
